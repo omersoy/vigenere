77fd39e62ab726b52da4f41f5843905300c7e1b2
unknown <omersoy93@gmail.com>
2020-05-22 04:21:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+// değişkenleri başlatıyoruz...
+$pswd = "";
+$code = "";
+$error = "";
+$valid = true;
+$color = "white";
+// form gönderildiyse
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+{
+	// şifrele ve şifrelenmişleri çöz
+	require_once('vigenere.php');
+	
+	// değişkenleri ayarla
+	$pswd = $_POST['pswd'];
+	$code = $_POST['code'];
+	
+	// şifreninin yani keyin sağlanıp sağlanmadığını kontrol ediyoruz.
+	if (empty($_POST['pswd']))
+	{
+		$error = "Lütfen key giriniz!";
+		$valid = false;
+		$color = "white";
+	}
+	
+	// metin olup olmadığını kontrol ediyoruz
+	else if (empty($_POST['code']))
+	{
+		$error = "Anahtar kelime girin!";
+		$valid = false;
+	}
+	
+	// şifrenin(keyin) alphanumeric olup olmadığını kontrol ediyoruz
+	else if (isset($_POST['pswd']))
+	{
+		if (!ctype_alpha($_POST['pswd']))
+		{
+			$error = "Şifre sadece alfabetik karakterler içermelidir!";
+			$valid = false;
+			$color = "white";
+		}
+	}
+	
+	// girilen değerler geçerliyse
+	if ($valid)
+	{
+		// eğer encrypt butonuna tıklandıysa
+		if (isset($_POST['encrypt']))
+		{
+			$code = encrypt($pswd, $code);
+			$error = "Metin başarıyla şifrelendi!";
+			$color = "white";
+		}
+			
+		// eğer decrypt button butonuna tıklandıysa
+		if (isset($_POST['decrypt']))
+		{
+			$code = decrypt($pswd, $code);
+			$error = "Kod başarıyla çözüldü!";
+			$color = "white";
+		}
+	}
+}
+?>
+
+<html>
+	<head>
+		<title>ISE426 - Vigenere Şifreleme</title>
+		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script type="text/javascript" src="Script.js"></script>
+	</head>
+	<body>
+		<br><br><br>
+		<form action="index.php" method="post">
+			<table cellpadding="5" align="center" cellpadding="2" border="7">
+				<caption><hr><b>ISE426 - Murat Koyuncu</b><hr></caption>
+				<tr>
+					<td align="center">Anahtar Kelime: <input type="text" name="pswd" id="pass" placeholder="Anahtar kelime giriniz" value="<?php echo htmlspecialchars($pswd); ?>" /></td>
+				</tr>
+				<tr>
+					<td align="center"><textarea id="box" name="code" placeholder="Plaintext(s) giriniz"><?php echo htmlspecialchars($code); ?></textarea></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="encrypt" class="button" value="ŞİFRELE" onclick="validate(1)" /></td>
+				</tr>
+				<tr>
+					<td><input type="submit" name="decrypt" class="button" value="ŞİFREYİ ÇÖZ" onclick="validate(2)" /></td>
+				</tr>
+				<tr>
+					<td align="center">ISE426 | VIGENERE | Copyright &copy; 2019 | OMER - TAHA</span></td>
+				</tr>
+				<tr>
+					<td><center><div style="color: <?php echo htmlspecialchars($color) ?>"><?php echo htmlspecialchars($error) ?></div></center></td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
---
+++ vigenere.php
@@ -0,0 +1,104 @@
+<?php
+// verilen metni şifrelemek için fonksiyon
+function encrypt($pswd, $text)
+{
+	// anahtarı(key) küçük harfe çevirme
+	$pswd = strtolower($pswd);
+	
+	// değişkenleri başlatıyoruz...
+	$code = "";
+	$ki = 0;
+	$kl = strlen($pswd);
+	$length = strlen($text);
+	
+	// metindeki her satırın üzerinde yineleme
+	for ($i = 0; $i < $length; $i++)
+	{
+		// harf alfa ise, şifreleyin
+		if (ctype_alpha($text[$i]))
+		{
+			// büyük harf
+			if (ctype_upper($text[$i]))
+			{
+				$text[$i] = chr(((ord($pswd[$ki]) - ord("a") + ord($text[$i]) - ord("A")) % 26) + ord("A"));
+			}
+			
+			// küçük harf
+			else
+			{
+				$text[$i] = chr(((ord($pswd[$ki]) - ord("a") + ord($text[$i]) - ord("a")) % 26) + ord("a"));
+			}
+			
+			// anahtar dizinini güncelliyoruz...
+			$ki++;
+			if ($ki >= $kl)
+			{
+				$ki = 0;
+			}
+		}
+	}
+	
+	// şifreli kodu dönderiyoruz
+	return $text;
+}
+// verilen metnin şifresini çözme foksiyonu(decode)
+function decrypt($pswd, $text)
+{
+	// anahtarı(key) küçük harfe çevirme
+	$pswd = strtolower($pswd);
+	
+	// intialize variables
+	$code = "";
+	$ki = 0;
+	$kl = strlen($pswd);
+	$length = strlen($text);
+	
+	// metindeki her satırın üzerinde yineleme
+	for ($i = 0; $i < $length; $i++)
+	{
+		// eğer harf ise şifresini çöz(decode)
+		if (ctype_alpha($text[$i]))
+		{
+			// büyük harf
+			if (ctype_upper($text[$i]))
+			{
+				$x = (ord($text[$i]) - ord("A")) - (ord($pswd[$ki]) - ord("a"));
+				
+				if ($x < 0)
+				{
+					$x += 26;
+				}
+				
+				$x = $x + ord("A");
+				
+				$text[$i] = chr($x);
+			}
+			
+			// küçük harf
+			else
+			{
+				$x = (ord($text[$i]) - ord("a")) - (ord($pswd[$ki]) - ord("a"));
+				
+				if ($x < 0)
+				{
+					$x += 26;
+				}
+				
+				$x = $x + ord("a");
+				
+				$text[$i] = chr($x);
+			}
+			
+			// anahtar dizinini güncelliyoruz
+			$ki++;
+			if ($ki >= $kl)
+			{
+				$ki = 0;
+			}
+		}
+	}
+	
+	// şifresi çözülen metni döndürdük
+	return $text;
+}
+?>
